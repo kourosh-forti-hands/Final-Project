7a61cb78842f42ae00fd770c2a3de9b6cde3cf87
kouroshmobl <moblkourosh@gmail.com>
2017-07-27 10:17:38 -0500

    Initial Commit

---
+++ Final Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Final Project
+//
+//  Created by Kourosh Mobl on 7/27/17.
+//  Copyright © 2017 Kourosh Mobl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
